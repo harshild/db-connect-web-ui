436637a4c5171a2228cf9253c52699ec06c99ca4
Harshil Dhariwal <harshil.dhariwal@volkswagen.co.in>
2017-01-20 13:33:28 +0530

    Init Project Structure

---
+++ src/main/java/com/github/harshil/SampleClass.java
@@ -0,0 +1,7 @@
+package com.github.harshil;
+
+/**
+ * Created by W18NM36 on 1/20/2017.
+ */
+public class SampleClass {
+}
---
+++ src/test/java/SampleTestClass.java
@@ -0,0 +1,5 @@
+/**
+ * Created by W18NM36 on 1/20/2017.
+ */
+public class SampleTestClass {
+}
