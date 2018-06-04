22348d130077425b010d123e294793a18dcb1d2c
Harshil Dhariwal <harshil.dhariwal@gmail.com>
2018-06-04 09:45:03 +0530

    Angular app init

--- scrapped/src/main/java/com/github/harshil/constant/URIConstants.java
+++
@@ -1,8 +0,0 @@
-package com.github.harshil.constant;
-
-/**
- * Created by W18NM36 on 1/25/2017.
- */
-public class URIConstants {
-    public static final String TEST_CONNECTION_URI = "/testcon";
-}
